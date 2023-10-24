520d41a4e1c53bc89f4d51624cf3d864e65a8c98
pigpgw <133184988+pigpgw@users.noreply.github.com>
2023-10-24 16:31:30 +0900

    Delete ParkGeonWoo/input:output directory

--- ParkGeonWoo/input:output/10818.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const { type } = require('os');
-
-let input = require('fs').readFileSync('ex.txt').toString().split('\n');
-let numbers = input[1].split(' ').map((x) => Number(x));
-
-console.log(Math.min(...numbers), Math.max(...numbers));
--- ParkGeonWoo/input:output/2557.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let input = fs.readFileSync(__dirname + '\\input.txt').toString();
-// let input = fs.readFileSync('/dev/stdin').toString().trim();
-
-console.log("Hello World!");
