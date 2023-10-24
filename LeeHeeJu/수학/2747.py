c56253260a191ecd8342fb95a7ddf88201045423
heeju <hxxzxx506@gmail.com>
2023-10-24 23:35:00 +0900

    정렬

--- LeeHeeJu/수학/2747.py
+++ LeeHeeJu/수학/2747.py
@@ -1,8 +1,19 @@
 n = int(input())
-num=[0,1]
+# num=[0,1]
 
-for i in range(2,n+1):
-    num.append(num[i-1]+num[i-2]);
+# for i in range(2,n+1):
+#     num.append(num[i-1]+num[i-2]);
 
-print(num[n])
+# print(num[n])
 
+# F0=0,F1=1
+
+# Fn=Fn-1+Fn-2
+
+
+def fibo(n):
+    if n <= 1:
+        return n
+    return fibo(n-1)+fibo(n-2)
+
+print(fibo(n))
---
+++ LeeHeeJu/정렬/10814.py
@@ -0,0 +1,19 @@
+#나이순 정렬
+import sys
+
+n = int(sys.stdin.readline());
+
+people=[list(sys.stdin.readline().rstrip().split(" ")) for _ in range(n)]
+people.sort(key=lambda x: int(x[0]))
+for person in people:
+    print(" ".join(person))
+# age={}
+
+# for _ in range(n):
+#     key,value = sys.stdin.readline().rstrip().split(" ")
+#     age[key] = value
+
+# print(sorted(age.items())
+# )
+
+
