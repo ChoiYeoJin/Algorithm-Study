1393c1f39ceada4ad497d30c5c08fa576230b922
KJH910810 <37525284+KJH910810@users.noreply.github.com>
2023-11-14 18:41:33 +0900

    Dynamic Programming study

---
+++ KimJiHwan/DynamicProgramming/pibonacci_top_down.js
@@ -0,0 +1,10 @@
+const dp = [0];
+
+function fibonacci(num) {
+    if(num <= 2) return 1;
+    if(dp[num]) return dp[num];
+    dp[num] = fibonacci(num-1) + fibonacci(num-2);
+    return dp[num];
+}
+
+module.export = fibonacci(4);
